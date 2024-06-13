d9f639e5605b2946ac49d6c46b8b0850acdee4dc
ma.egorov <ma.egorov@tensor.ru>
2024-06-13 20:29:51 +0300

    убрал ненужный ключ у пользаков

--- mongoDB/models/user.ts
+++ mongoDB/models/user.ts
@@ -8,54 +8,57 @@ import {
    REG_EXP_RUS_WORD,
 } from '../../helpers/constants'
 
-const userScheme = new Schema({
-   login: {
-      type: String,
-      required: true,
-      minLength: MIN_LENGTH_LOGIN,
-      maxLength: MAX_LENGTH_LOGIN,
-      validate: (text: string) => REG_EXP_LOGIN.test(text),
-   },
-   email: {
-      type: String,
-      required: true,
-      validate: (text: string) => REG_EXP_EMAIL.test(text),
-   },
-   password: {
-      type: String,
-      required: true,
-      minLength: MIN_LENGTH_PASSWORD,
-   },
-   confirmAccount: {
-      type: Boolean,
-      required: true,
-   },
-   info: {
-      name: {
+const userScheme = new Schema(
+   {
+      login: {
          type: String,
          required: true,
-         validate: (text: string) => REG_EXP_RUS_WORD.test(text),
+         minLength: MIN_LENGTH_LOGIN,
+         maxLength: MAX_LENGTH_LOGIN,
+         validate: (text: string) => REG_EXP_LOGIN.test(text),
       },
-      surname: {
+      email: {
          type: String,
          required: true,
-         validate: (text: string) => REG_EXP_RUS_WORD.test(text),
+         validate: (text: string) => REG_EXP_EMAIL.test(text),
       },
-      birthday: Date,
-   },
-   personalization: {
-      avatar: String,
-      background: String,
-      status: String,
-      border: String,
-   },
-   collections: [
-      {
-         name: String,
-         values: [Number],
+      password: {
+         type: String,
+         required: true,
+         minLength: MIN_LENGTH_PASSWORD,
       },
-   ],
-})
+      confirmAccount: {
+         type: Boolean,
+         required: true,
+      },
+      info: {
+         name: {
+            type: String,
+            required: true,
+            validate: (text: string) => REG_EXP_RUS_WORD.test(text),
+         },
+         surname: {
+            type: String,
+            required: true,
+            validate: (text: string) => REG_EXP_RUS_WORD.test(text),
+         },
+         birthday: Date,
+      },
+      personalization: {
+         avatar: String,
+         background: String,
+         status: String,
+         border: String,
+      },
+      collections: [
+         {
+            name: String,
+            values: [Number],
+         },
+      ],
+   },
+   { versionKey: false }
+)
 
 userScheme.set('toJSON', {
    transform: function (doc, ret) {
