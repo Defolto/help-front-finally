59bcef6092b3247892cf399136cf73e74167eb26
ma.egorov <ma.egorov@tensor.ru>
2024-06-15 17:22:16 +0300

    инициализация стора

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,5 @@
 'use client'
 
-import { useEffect } from 'react'
-import { getCookie } from '../../helpers/cookie'
-
 export default function Home() {
-   useEffect(() => {
-      const id = getCookie('user_id')
-
-      if (!id) {
-         return
-      }
-
-      console.log(id)
-   }, [])
-
    return <span>Привет, главная страница</span>
 }
