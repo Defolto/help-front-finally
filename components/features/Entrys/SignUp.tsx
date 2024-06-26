7e6daa5552ee59ff7e80ce423ad24ccb52518192
ma.egorov <ma.egorov@tensor.ru>
2024-06-26 12:40:14 +0300

    автоотправка при полном вводе проверочного кода

--- components/features/Entrys/SignUp.tsx
+++ components/features/Entrys/SignUp.tsx
@@ -3,7 +3,7 @@ import { deleteCookie, getCookie, setCookie } from 'helpers/cookie'
 import { createFetch } from 'helpers/createFetch'
 import { getMass } from 'helpers/functions'
 import { useRouter } from 'next/navigation'
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useCallback, useEffect, useState } from 'react'
 import { useKeyboard } from 'store/keyboard'
 import { Button } from '../../ui/Button'
 import { Input } from '../../ui/Input'
@@ -44,26 +44,27 @@ export default function SignUp() {
          })
    }
 
-   const confirmCode = (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
+   const confirmCode = useCallback(
+      (code: string) => {
+         const id = getCookie('interim_id')
 
-      const code = codeConfirm.join('')
-      const id = getCookie('interim_id')
+         if (code.length !== 4) {
+            return
+         }
 
-      if (code.length !== 4) {
-         return
-      }
-
-      createFetch('api/user/confirm', { id, code })
-         .then((res) => {
-            setCookie('user_id', res.data)
-            deleteCookie('interim_id')
-            router.push('/profile')
-         })
-         .catch((e) => {
-            console.log(e)
-         })
-   }
+         createFetch('api/user/confirm', { id, code })
+            .then((res) => {
+               setCookie('user_id', res.data)
+               deleteCookie('interim_id')
+               router.push('/profile')
+            })
+            .catch((e) => {
+               // TODO: красивая обработка ошибки
+               console.log(e)
+            })
+      },
+      [router]
+   )
 
    const handleChangeName = (e: any) => {
       e.target.value = formatName(e.target.value)
@@ -89,9 +90,14 @@ export default function SignUp() {
             items.pop()
          }
 
+         // сразу отправляем проверку, если введён код
+         if (items.length === CODE_LENGTH) {
+            confirmCode(items.join(''))
+         }
+
          return items
       })
-   }, [keyboard])
+   }, [keyboard, confirmCode])
 
    return (
       <>
@@ -129,7 +135,13 @@ export default function SignUp() {
                </Button>
             </form>
          ) : (
-            <form className="flex flex-col" onSubmit={confirmCode}>
+            <form
+               className="flex flex-col"
+               onSubmit={(e: FormEvent<HTMLFormElement>) => {
+                  e.preventDefault()
+                  confirmCode(codeConfirm.join(''))
+               }}
+            >
                <p className="text-center text-xl font-bold text-white">Введите код с почты</p>
                <div className="flex">
                   {getMass(CODE_LENGTH).map((_, col) => {
