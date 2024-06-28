c53f42ae4662a57a4168113d48f4cc92410bc47f
ma.egorov <ma.egorov@tensor.ru>
2024-06-28 14:17:05 +0300

    Определение активного пункта меню

--- components/features/UserMenu/UserMenu.tsx
+++ components/features/UserMenu/UserMenu.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
 import Calendar from './icons/Calendar'
 import Collections from './icons/Collections'
 import Home from './icons/Home'
+import Materials from './icons/Materials'
 import School from './icons/School'
 import Settings from './icons/Settings'
 import Shop from './icons/Shop'
@@ -10,20 +13,27 @@ import Shop from './icons/Shop'
 type IMenuItem = {
    title: string
    icon: JSX.Element
+   href: string
 }
 
 const MENU_ITEMS: IMenuItem[] = [
-   { title: 'Главная', icon: <Home /> },
-   { title: 'Расписание', icon: <Calendar /> },
-   { title: 'Моя школа', icon: <School /> },
-   { title: 'Магазин', icon: <Shop /> },
-   { title: 'Коллекции', icon: <Collections /> },
-   { title: 'Настройки', icon: <Settings /> },
+   { title: 'Главная', icon: <Home />, href: 'profile' },
+   { title: 'Расписание', icon: <Calendar />, href: 'calendar' },
+   { title: 'Материалы', icon: <Materials />, href: 'materials' },
+   { title: 'Моя школа', icon: <School />, href: 'school' },
+   { title: 'Магазин', icon: <Shop />, href: 'shop' },
+   { title: 'Коллекции', icon: <Collections />, href: 'collections' },
+   { title: 'Настройки', icon: <Settings />, href: 'settings' },
 ]
 
-function UserMenuItem({ title, icon }: IMenuItem) {
+function UserMenuItem({ title, icon, active }: IMenuItem & { active: boolean }) {
    return (
-      <div className="flex cursor-pointer flex-row items-center gap-2">
+      <div
+         className={clsx(
+            'flex cursor-pointer flex-row items-center gap-2 hover:text-green',
+            active && 'text-green'
+         )}
+      >
          {icon}
          <p>{title}</p>
       </div>
@@ -31,10 +41,17 @@ function UserMenuItem({ title, icon }: IMenuItem) {
 }
 
 export default function UserMenu() {
+   const pathname = usePathname()
+   const lastPathname = pathname.split('/').pop()
+
    return (
-      <div className="bg-secondBg box-border flex h-full flex-col gap-4 p-4">
+      <div className="box-border flex h-full flex-col gap-4 bg-secondBg p-4">
          {MENU_ITEMS.map((item, index) => (
-            <UserMenuItem key={`user-menu-${index}`} {...item} />
+            <UserMenuItem
+               key={`user-menu-${index}`}
+               {...item}
+               active={lastPathname === item.href}
+            />
          ))}
       </div>
    )
