e9513b98a5c221ea9595c84c2a8e6fe9103d3770
hoprik <42morozov42@gmail.com>
2024-06-30 13:41:16 +0300

    Сделал выбор темы, добавил тестовый md файл и обновил npm (случайно)

--- app/materials/[subject]/page.tsx
+++ app/materials/[subject]/page.tsx
@@ -1,10 +1,51 @@
+import {closeDB, openDB} from "mongoDB/general";
+import Subject from "mongoDB/models/subject";
+import { MDXRemote } from 'next-mdx-remote/rsc'
+
 type Props = {
    params: {
       subject: string
    }
 }
 
+type ThemeElement = {
+   name: string,
+   href: string
+}
+
+
+async function getThemes(subjectName: string): Promise<ThemeElement[]>{
+   await openDB();
+
+   const subject = await Subject.findOne({ title: subjectName });
+   if (!subject){
+      return []
+   }
+
+   await closeDB();
+
+   return subject["themes"]
+}
+
 
 export default async function Theme({ params: { subject } }: Props) {
-   return <div className="flex flex-col">{subject}</div>
+   const themes: ThemeElement[] = await getThemes(subject);
+   return <>
+      <div className="flex-col">
+         <div>
+            <MDXRemote source={"# Тестовый ГЛАВНЫЙ.md файл"}/>
+         </div>
+         <div className="flex flex-col">
+            {
+               themes.length == 0 ?
+                   <p>Ошибка, предмет не найден!</p> :
+                   <>
+                      {themes.map((value, key) => {
+                         return <a key={key} href={value.href}>{value.name}</a>
+                      })}
+                   </>
+            }
+         </div>
+      </div>
+   </>
 }
